0bd62ba6a8655b66e5d7116cc7a9a430e4c31313
Danny Becerra <ddnielbec@outlook.com>
2022-07-28 14:28:27 -0500

    last details

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelector('.nav-links');
+const hamburguer = document.querySelector('.hamburguer');
 
 
 navToggle.addEventListener('click', function () {
     navLinks.classList.toggle('show-links');
+    hamburguer.classList.toggle('btn-clicked')
 })
