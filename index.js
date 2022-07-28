3e10ddba065843232cf88cd2e70dd89392bb0190
Danny Becerra <ddnielbec@outlook.com>
2022-07-28 14:13:47 -0500

    Simple nav bar

---
+++ index.js
@@ -0,0 +1,9 @@
+//Seleccion de elementos
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+
+navToggle.addEventListener('click', function () {
+    navLinks.classList.toggle('show-links');
+})
